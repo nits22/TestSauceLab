c37714b76d5ccc1031f44d26d89bcd5ce75d0a97
nits22 <nitishbector.it@gmail.com>
2016-09-18 02:09:16 +0530

    sauce

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,35 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+
+public class ExampleTest {
+
+	@Test
+	public void app1() throws MalformedURLException
+	{
+		  final String USERNAME = "nits";
+		  final String ACCESS_KEY = "1e9d3e5a-c560-4dda-814d-88d698895884";
+		  final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+		  DesiredCapabilities caps = DesiredCapabilities.chrome();
+		    caps.setCapability("platform", "Windows XP");
+		    caps.setCapability("version", "43.0");
+
+		    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+
+		    /**
+		     * Goes to Sauce Lab's guinea-pig page and prints title
+		     */
+
+		    driver.get("https://www.facebook.com/");
+		    System.out.println("title of page is: " + driver.getTitle());
+
+		    driver.quit();
+		
+	}
+}
